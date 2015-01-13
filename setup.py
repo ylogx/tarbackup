508b9483fd61721d04235b6fa40b7b5af3b25b3f
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-01-13 17:59:19 +0530

    Inital commit with files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+add_keywords = dict(
+    entry_points = {
+        'console_scripts': ['tarbackup = tarbackup.tar_backup:main'],
+    },
+)
+
+setup(
+        name='tarbackup',
+        description='Create unique tar backups',
+        version='0.0.1',
+        packages=['tarbackup'],
+        license='GPLv3+',
+        author='Shubham Chaudhary',
+        author_email='me@shubhamchaudhary.in',
+        url='https://github.com/shubhamchaudhary/tarbackup',
+        long_description=open('README.txt').read(),
+        **add_keywords
+)
+

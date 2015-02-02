18a321f7f7ed211afafca29300f6040fd51f1f62
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-02-02 17:57:52 +0530

    Close files properly and added requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ add_keywords = dict(
     },
 )
 
+fhan = open('requirements.txt', 'rU')
+requires = [line.strip() for line in fhan.readlines()]
+fhan.close()
+#print('We require: ', requires)
+fhan = open('README.txt')
+long_description = fhan.read()
+fhan.close()
+
 setup(
         name='tarbackup',
         description='Create unique tar backups',
@@ -15,7 +23,8 @@ setup(
         author='Shubham Chaudhary',
         author_email='me@shubhamchaudhary.in',
         url='https://github.com/shubhamchaudhary/tarbackup',
-        long_description=open('README.txt').read(),
+        long_description=long_description,
+        install_requires=requires,
         **add_keywords
 )
 
